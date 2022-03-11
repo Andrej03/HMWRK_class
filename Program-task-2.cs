0bf02390e83c1de5c2c403c091a536682e8bb2a9
Andrej03 <96391874+Andrej03@users.noreply.github.com>
2022-03-11 16:46:05 +0100

    Add files via upload

---
+++ Program-task-2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HMWRK_class_2_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 15;
+            int c = 20;
+            int d = 30;
+
+            int avrg = ((a + b + c + d) / 4);
+
+            Console.WriteLine(avrg);
+        }
+    }
+}
