f545847c10f1f87f4acce33a2d243ae105a4836b
Andrej03 <96391874+Andrej03@users.noreply.github.com>
2022-03-11 16:45:00 +0100

    Add files via upload

---
+++ Program-task-1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HMWRK_class_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int m = 15;
+            int sum = n + m;
+            Console.WriteLine(sum);
+        }
+    }
+}
